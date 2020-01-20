c4d328a060b7d33e2b2b8a8aba343dcabb7eccc4
Fabricio Paiva <40863175+fabricio24530@users.noreply.github.com>
2020-01-20 11:17:28 -0300

    Create EstruturaSequencial03.py

---
+++ EstruturaSequencial03.py
@@ -0,0 +1,6 @@
+'''Faça um Programa que peça dois números e imprima a soma.'''
+
+n1, n2 = int(input('Informe o primeiro numero: ), int(input('informe o segundo numero: '))
+soma = n1 + n2
+
+print(f'A soma de {n1} e {n2} é {soma}')
