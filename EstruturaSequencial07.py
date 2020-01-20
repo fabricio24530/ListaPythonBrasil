d57e45fb90c7045de1b077b04813ac9c4ff4ddce
Fabricio Paiva <40863175+fabricio24530@users.noreply.github.com>
2020-01-20 11:33:35 -0300

    Create EstruturaSequencial07.py

---
+++ EstruturaSequencial07.py
@@ -0,0 +1,7 @@
+'''Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário'''
+
+lado = float(input('Informe o tamanho do lado do quadrado: '))
+area = pow(lado, 2)
+
+print(f'O dobro da area do quado é {area * 2}u²')
+
