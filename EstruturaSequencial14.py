45ce44a0918f0c737732061e8b77193a8e107301
Fabricio Paiva <40863175+fabricio24530@users.noreply.github.com>
2020-01-20 12:50:10 -0300

    Create EstruturaSequencial14.py

---
+++ EstruturaSequencial14.py
@@ -0,0 +1,19 @@
+'''João Papo-de-Pescador, homem de bem, comprou um microcomputador para controlar o rendimento diário de seu trabalho.
+Toda vez que ele traz um peso de peixes maior que o estabelecido pelo regulamento de pesca do estado de
+São Paulo (50 quilos) deve pagar uma multa de R$ 4,00 por quilo excedente. João precisa que você faça um programa que
+leia a variável peso (peso de peixes) e calcule o excesso. Gravar na variável excesso a quantidade de quilos além do
+limite e na variável multa o valor da multa que João deverá pagar. Imprima os dados do programa com as mensagens
+adequadas.'''
+
+peso = float(input('Informe a quantidade de quilos de peixe: '))
+excesso_de_quilos = peso - 50
+multa = excesso_de_quilos * 4
+
+if excesso_de_quilos <= 0:
+    print(f'''Quantidade de peixe: {peso}Kg
+              Excesso de peixe: -
+              Multa: -''')
+elif excesso_de_quilos > 0:
+    print(f'''Quantidade de peixe: {peso}Kg
+                  Excesso de peixe: {excesso_de_quilos}Kg
+                  Multa: R$ {multa}''')
