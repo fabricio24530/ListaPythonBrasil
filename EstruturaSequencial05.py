13974aaf35bbfd4076618e2d20615e5a1cc22d0e
Fabricio Paiva <40863175+fabricio24530@users.noreply.github.com>
2020-01-20 11:27:49 -0300

    Create EstruturaSequencial05.py

---
+++ EstruturaSequencial05.py
@@ -0,0 +1,7 @@
+'''Faça um Programa que converta metros para centímetros.'''
+
+metros = float(input('Informe a metragem em metros: '))
+
+cetimetros = metros * 100
+
+print(f'{metros}m equivale(m) a {cetimetros}cm')
