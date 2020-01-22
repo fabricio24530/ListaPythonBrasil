8b1cc7c7e7c6a9a5f7e33f69f7515a6c94c529a1
Fabricio Paiva <40863175+fabricio24530@users.noreply.github.com>
2020-01-22 12:38:08 -0300

    Create EstruturaDeDecisao02.py

---
+++ EstruturaDeDecisao02.py
@@ -0,0 +1,8 @@
+'''Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.'''
+valor = float(input('Informe um numero qualquer: '))
+if valor > 0:
+    print('Positivo')
+elif valor < 0:
+    print('Negativo')
+else:
+    print('Nulo')
