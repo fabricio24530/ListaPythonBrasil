6213b92d04fd130ceacf9703a41b8a3140d3feae
Fabricio Paiva <40863175+fabricio24530@users.noreply.github.com>
2020-01-26 20:18:38 -0300

    Create EstruturaDeRepeticao48.py

---
+++ EstruturaDeRepeticao48.py
@@ -0,0 +1,8 @@
+'''Faça um programa que peça um numero inteiro positivo e em seguida mostre este numero invertido.
+Exemplo:
+  12376489
+  => 98467321'''
+
+num = input('Informe um numero inteiro positivo: ')
+
+print('=>',num[::-1])  # Metodo(Slice) que inverte um inteiravel qualquer
