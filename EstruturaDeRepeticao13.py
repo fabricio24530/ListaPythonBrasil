fb783cdfb75c93053ccaba93e4c3163bfab1d0ad
Fabricio Paiva <sdpaiva24530@gmail.com>
2020-01-26 20:37:37 -0300

    falta terminar

---
+++ EstruturaDeRepeticao13.py
@@ -0,0 +1,16 @@
+'''Faça um programa que peça dois números, base e expoente, calcule e mostre o primeiro número elevado ao segundo número.
+Não utilize a função de potência da linguagem.'''
+
+base = float(input('Informe a base: '))
+expoente = int(input('Informe o expoente: '))
+resultado = base
+
+for i in range(expoente):
+    resultado = resultado * resultado
+
+
+
+
+print(resultado)
+
+'''0 , 1, 2 ==> 1, 2 , 3'''
