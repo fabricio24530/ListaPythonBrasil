0e4293b56e16decec67faae2b3582498316422e6
Fabricio Paiva <40863175+fabricio24530@users.noreply.github.com>
2020-01-26 15:15:16 -0300

    Create EstruturaDeDecisao22.py

---
+++ EstruturaDeDecisao22.py
@@ -0,0 +1,8 @@
+'''Faça um Programa que peça um número inteiro e determine se ele é par ou impar. '''
+
+num = int(input('Informe um numero: '))
+
+if num % 2 == 0:
+    print(f'{num} é par')
+else:
+    print(f'{num} é impar')
