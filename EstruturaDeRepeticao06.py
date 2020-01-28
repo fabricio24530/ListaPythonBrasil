623d3c6a7777a857c3c7ccdd05ff739f0cfb0f07
Fabricio Paiva <sdpaiva24530@gmail.com>
2020-01-28 15:08:21 -0300

    Resolvido

---
+++ EstruturaDeRepeticao06.py
@@ -0,0 +1,13 @@
+'''Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa para que
+ele mostre os números um ao lado do outro.'''
+
+for i in range(1, 21):
+    print(i)
+
+lista = list()
+
+for i in range(1, 21):
+    lista.append(i)
+
+print(lista)
+
